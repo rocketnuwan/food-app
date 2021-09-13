98e9864ccc877b9ad633a551ca2af940176fabac
rocketnuwan <rocketnuwan.tharanga99@gmail.com>
2021-09-13 21:45:12 +0530

    splash screen added

---
+++ lib/screens/splash_screen/splash_screen.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:food_app/screens/splash_screen/getting_started.dart';
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({Key? key}) : super(key: key);
+
+  @override
+  _SplashScreenState createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    super.initState();
+    Future.delayed(const Duration(seconds: 5), () {
+      Navigator.push(
+          context,
+          MaterialPageRoute(
+            builder: (context) => const GettingStarted(),
+          ));
+    });
+  }
+
+  Widget build(BuildContext context) {
+    final size = MediaQuery.of(context).size;
+    return Scaffold(
+      body: Container(
+        width: size.width,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Image.asset("assets/images/food.png"),
+            const SizedBox(
+              height: 8,
+            ),
+            const Text(
+              "No waiting for food",
+              style: TextStyle(
+                fontSize: 14,
+                fontWeight: FontWeight.w400,
+                color: Color(0xff838383),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
