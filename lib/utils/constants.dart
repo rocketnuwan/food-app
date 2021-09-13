04ae1323c4c895b5c3406dcaf18e13b83a1ce4a1
rocketnuwan <rocketnuwan.tharanga99@gmail.com>
2021-09-13 22:49:48 +0530

    add image call functions

---
+++ lib/utils/constants.dart
@@ -0,0 +1,10 @@
+class Constants {
+  //assets paths
+  static const IMAGE_PATH = 'assets/images/';
+  static const ICON_PATH = 'assets/icons/';
+
+  //image assets funstion
+  static String imageAsset(img) => "$IMAGE_PATH$img";
+  //image assets funstion
+  static String iconAsset(img) => "$ICON_PATH$img";
+}
